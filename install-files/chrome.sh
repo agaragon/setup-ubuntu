4d8e25d1cd2ffbe6a84e50bbca3f69f590922712
André Guimarães Aragon <andregaragon@gmail.com>
2024-06-15 09:57:35 -0300

    initial commit

---
+++ install-files/chrome.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt upgrade
+if [ $(command -v google-chrome | grep / -c) ]
+then
+    echo 'chrome is already installed'
+elif [ $(ls | grep google-chrome -c -eq 0) ]
+then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    echo 'chrome package not found, downloading it to install'
+    rm google-chrome-stable_current_amd64.deb
+else
+    echo 'chrome package found, installing it instead of downloading it'
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+fi
---
+++ install-files/git.sh
@@ -0,0 +1,4 @@
+sudo apt install git -y
+
+git config --global user.email "andregaragon@gmail.com"
+git config --global user.name "André Guimarães Aragon"
