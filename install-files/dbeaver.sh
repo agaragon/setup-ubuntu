402cb2c683f4f2b6a88769c21354342b8e7158c3
André Guimarães Aragon <andregaragon@gmail.com>
2024-08-09 07:55:30 -0300

    dbeaver installation added

---
+++ install-files/dbeaver.sh
@@ -0,0 +1,3 @@
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+sudo apt-get install dbeaver-ce
